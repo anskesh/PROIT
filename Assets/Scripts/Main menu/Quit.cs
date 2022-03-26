bb10c3ab6a57afb71ab567b96233552208d3d198
anskesh <72143945+anskesh@users.noreply.github.com>
2022-03-26 21:12:54 +0300

    Добавлены новые спрайты, заменены предыдущие

--- Assets/Scripts/Main menu/Quit.cs
+++ Assets/Scripts/Main menu/Quit.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void Exit()
+	{
+		Application.Quit();
+	}
 }
